3ba90f1522bb6045d1258598e9ca100d330aed73
CESAR NUNES DE OLIVEIRA <ra22139193-5@aluno.unicesumar.edu.br>
2023-05-28 08:18:32 -0300

    Arquivos Iniciais

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript code for infinite scrolling
+let newsContainer = document.getElementById("news-container");
+let newsContent = document.getElementById("news-content");
+
+newsContainer.addEventListener("scroll", function () {
+  if (
+    newsContainer.scrollTop + newsContainer.clientHeight >=
+    newsContent.offsetHeight
+  ) {
+    // When the user reaches the bottom of the container, add more news content
+    newsContent.innerHTML += newsContent.innerHTML; // Append the existing news content
+  }
+});
